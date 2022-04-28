884f8fb6f7f124b83aacf6e402cc3c4f887a7177
madhavpasumarthy <psvsaimadhav@gmail.com>
2022-04-29 00:28:06 +0530

    database.js created

---
+++ server/dataBase.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('memorygame', 'sampleuser', 'game', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+  }
+});
+
+// intializing the user table
+const Users = sequelize.define('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement:true,
+      allowNull:false,
+      primaryKey:true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique: true
+    }, 
+    Firstname: {
+      type: Sequelize.STRING,
+      allowNuLL:false
+    },
+    Lastname:{
+        type: Sequelize.STRING,
+        allowNuLL:false
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNuLL:false
+    }
+  });
+
+sequelize.sync()
+  .then(()=>
+    {
+     console.log("database has synced");
+    }
+  )
+  .catch((err)=>
+    {
+        console.error("error creating database");
+    }   
+  )
+
+  
+module.exports={
+    Users
+}
+
