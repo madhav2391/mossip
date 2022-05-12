21d33bafe8258c662a3cae91dee2ed1510cfc127
madhav2391 <44904272+madhav2391@users.noreply.github.com>
2022-05-12 23:40:55 +0530

    Update dataBase.js

--- server/dataBase.js
+++ server/dataBase.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 // const {Logger} = require("./logger");
 var Logger = require('./logger')
 
-const sequelize = new Sequelize('memorygame1', 'root', 'password', {
+const sequelize = new Sequelize('memorygame', 'root', 'password', {
   host: 'mysqldb',
   dialect: 'mysql',
   operatorsAliases: false,
